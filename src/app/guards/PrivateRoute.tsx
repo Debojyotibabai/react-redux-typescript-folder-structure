22fbe0aa4a605a282c7384929de845a59c13553d
Debojyotibabai <debojyotighosh060@gmail.com>
2022-08-18 15:06:57 +0530

    fix

--- src/app/guards/PrivateRoute.tsx
+++ src/app/guards/PrivateRoute.tsx
@@ -1,14 +1,4 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useAppSelector } from 'store';
 
-export const PrivateRoute = () => {
-  const auth = useAppSelector((state) => state.auth);
-  const user = useAppSelector((state) => state.user);
-  const location = useLocation();
-
-  if (auth?.isLoggedIn && user) {
-    return <Outlet />;
-  }
-
-  return <Navigate to="/auth/signin" state={{ from: location }} replace />;
-};
+export const PrivateRoute = () => {};
